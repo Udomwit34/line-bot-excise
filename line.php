b40641cc12c84be20366dae52312f4ca8ecd98dc
Udomwit34 <68369749+Udomwit34@users.noreply.github.com>
2020-07-16 15:23:58 +0700

    Update line.php

--- line.php
+++ line.php
@@ -59,7 +59,12 @@ if ( sizeof($request_array['events']) > 0 )
 	   if($text == "ใบอนุญาตขายสุราของฉันหมดอายุวันไหน"){
 		   $reply_message = 'ฉันขอเลขที่ใบอนุญาตของคุณเพื่อทำการตรวจสอบ';
 	   }	   
+	    if($text == "เลิกอบรมกี่โมง"){
+		   $reply_message = '4 โมงก็ควรเลิกนะ';
+	   }	    
+	   
 	    
+	   
 	   $split_text = explode(" ", $text);
 	   //foreach ($split_text as $t){$reply_message = $t;}
 	   if (in_array("ฉันหาเลขใบอนุญาตของฉันไม่เจอ", $split_text)) { 
